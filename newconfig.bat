904d8eee6553c5911b1a5fee75b80b208694c7c2
FlamboyantAnon <130598389+Flamanon@users.noreply.github.com>
2023-06-15 21:44:13 +0530

    Update newconfig.bat

--- newconfig.bat
+++ newconfig.bat
@@ -1,7 +1,20 @@
 @echo off
+
+REM Install Node.js modules
+npm install
+
+REM Start the server
 start "node" /min cmd /C node server.cjs
+
+REM Wait for 5 seconds
 timeout /t 5 /nobreak >nul
+
+REM Open the URL in the default browser
 start http://localhost:3000
+
+REM Prompt the user to stop the server
 echo Press any key to stop the server...
 pause >nul
+
+REM Terminate the server process
 taskkill /IM node.exe /F
