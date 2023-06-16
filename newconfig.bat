addf9fcdf04ddab114aaba8fe5a4840f54efe613
FlamboyantAnon <130598389+Flamanon@users.noreply.github.com>
2023-06-16 07:48:43 +0530

    Update newconfig.bat

--- newconfig.bat
+++ newconfig.bat
@@ -1,6 +1,6 @@
 @echo off
 
-npm install
+call npm install
 
 start "node" /min cmd /C node server.cjs
 
@@ -8,9 +8,7 @@ timeout /t 5 /nobreak >nul
 
 start http://localhost:3000
 
-
 echo Press any key to stop the server...
 pause >nul
-
 
 taskkill /IM node.exe /F
